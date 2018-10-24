0343e83f441adb90f6c43c293980726d2ec27981
Leslie Richardson <lerich@microsoft.com>
2018-10-24 09:42:20 -0700

    Added lightweight console app for loading recipes

---
+++ src/ConsoleRecipes/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Xml;
+
+namespace ConsoleRecipes
+{
+    class Program
+    {
+        private string RecipesPath = Path.GetFullPath("App_Data/Recipes");
+        private Dictionary<long?, Recipe> Recipes = new Dictionary<long?, Recipe>();
+        private Random rand = new Random();
+
+        public Program() {
+
+            foreach (string fileName in Directory.GetFiles(RecipesPath))
+            {
+                string json = File.ReadAllText(fileName);
+                Recipe recipe = LoadRecipeFromJson(json);
+                recipe.Hits = 0; //initialize # of clicks to 0 for each recipe
+                Recipes.Add(recipe.Id, recipe);
+            }
+            keysEnumerator = Recipes.Keys.GetEnumerator();
+        }
+
+        private Recipe LoadRecipeFromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Recipe>(json, Converter.Settings);
+        }
+
+        private IEnumerator<long?> keysEnumerator;
+        public Recipe NextRecipe
+        {
+            get
+            {
+                if (!keysEnumerator.MoveNext())
+                {
+                    keysEnumerator.Reset();
+                }
+
+                return Recipes[keysEnumerator.Current];
+            }
+            set { }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+        }
+    }
+}
